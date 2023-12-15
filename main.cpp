d41dcbe23409d674108b670e5d73206160b929c5
U-BSD\480143 <480143@SHS-8YBF6X3.beaverton.k12.or.us>
2023-12-15 09:08:28 -0800

    Lots of errors but starting to get recursion

--- main.cpp
+++ main.cpp
@@ -6,10 +6,40 @@
 using namespace std;
 
 int main(){
-
+  Node* head;
 
 
 
 
   return 0;
+}
+
+void add(Node* &head, Node* current, Node* prev){
+  cout << "Input the user's ID" << endl;
+  int tempID;
+  cin >> tempID;
+  student* newstudent = new Student();
+  newstudent->setID(tempID);
+  if(head == NULL){
+    head = new Node();
+    head->setStudent(newstudent);
+  }
+  else{
+    Node* tempNode = new Node();
+    tempNode.setStudent(newstudent);
+    if(prev->getStudent()->getID() < tempID && current->getStudent()->getID() > tempID){
+      prev->setNext(tempNode);
+      tempNode->setNext(current);
+      
+    }
+
+
+
+
+  }
+  
+
+
+
+
 }
